5b6d4f0a696b27e827cf0689f4e5f8d929c23985
Kelechi <kellyxglobal@gmail.com>
2023-09-15 21:30:24 +0100

    Creating a controller for home page and cart page

---
+++ app/Http/Controllers/PagesController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class PagesController extends Controller
+{
+    // Home
+    public function home()
+    {
+        echo "Home";
+    }
+
+    // Cart
+    public function cart()
+    {
+        echo "Cart";
+    }
+
+}
